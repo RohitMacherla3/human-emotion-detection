2b1f56387f42dcc058ce0c84e8730d3319a8e99f
Rohit Macherla <rm1667@scarletmail.rutgers.edu>
2024-02-08 21:42:48 -0500

    Update main.py

--- service/main.py
+++ service/main.py
@@ -2,7 +2,6 @@ from fastapi import FastAPI
 from service.api.api import api_router
 import onnxruntime as rt
 import os
-import uvicorn as uvi
 
 app = FastAPI(name='Human Emotions Detection')
 app.include_router(api_router)
@@ -13,7 +12,3 @@ ViT_model = rt.InferenceSession('Model/ViT_quantized.onnx', providers=providers)
 @app.get('/')
 async def root():
     return 'Welcome!'
-
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8000))
-    uvi.run(app, host="0.0.0.0", port=port)
